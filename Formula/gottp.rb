77d260929707c30b8926101114d3793bad674f6a
sadopc <98687378+sadopc@users.noreply.github.com>
2026-02-11 15:36:51 +0300

    feat: update gottp formula to v0.5.0 with pre-built binaries

--- Formula/gottp.rb
+++ Formula/gottp.rb
@@ -1,19 +1,33 @@
 class Gottp < Formula
   desc "A Postman/Insomnia-like TUI API client built in Go"
   homepage "https://github.com/sadopc/gottp"
+  version "0.5.0"
   license "MIT"
-  head "https://github.com/sadopc/gottp.git", branch: "main"
 
-  depends_on "go" => :build
+  on_macos do
+    on_intel do
+      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_darwin_amd64.tar.gz"
+      sha256 "4c83a436d9459b9dfa5549ece3014c343678612d5118b4a4444130136f635de7"
+    end
+    on_arm do
+      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_darwin_arm64.tar.gz"
+      sha256 "057ea2d527f0d6952e73cdd5228f252d7b04c349069d2364e052e9f026122644"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_linux_amd64.tar.gz"
+      sha256 "016fbb818831c630ad9b7c22367fdc0e8b9ab76e6988e65084013fcc2575e670"
+    end
+    on_arm do
+      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_linux_arm64.tar.gz"
+      sha256 "65ead4038653b801b93d08510a795067859615544b19f37366b6c76d04c2d165"
+    end
+  end
 
   def install
-    ldflags = %W[
-      -s -w
-      -X github.com/serdar/gottp/pkg/version.Version=#{version}
-      -X github.com/serdar/gottp/pkg/version.Commit=HEAD
-      -X github.com/serdar/gottp/pkg/version.Date=#{time.iso8601}
-    ]
-    system "go", "build", *std_go_args(ldflags:), "./cmd/gottp"
+    bin.install "gottp"
   end
 
   test do
