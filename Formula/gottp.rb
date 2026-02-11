8a4236ece3eed99db71fa4913339301432725a30
sadopc <98687378+sadopc@users.noreply.github.com>
2026-02-11 19:20:47 +0300

    Update gottp formula to v1.0.0

--- Formula/gottp.rb
+++ Formula/gottp.rb
@@ -1,28 +1,28 @@
 class Gottp < Formula
   desc "A Postman/Insomnia-like TUI API client built in Go"
   homepage "https://github.com/sadopc/gottp"
-  version "0.5.0"
+  version "1.0.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_darwin_amd64.tar.gz"
-      sha256 "c91ea96cc4582f66ef45494969960a1d90df683fbb8797caab7d5970bb92274f"
+      url "https://github.com/sadopc/gottp/releases/download/v1.0.0/gottp_1.0.0_darwin_amd64.tar.gz"
+      sha256 "500a83358b87f3ab0fa401fc4561a0feed4799113efdaf5c9abbd1c771805c19"
     end
     on_arm do
-      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_darwin_arm64.tar.gz"
-      sha256 "49523af50acd8a8881d2cdbad57255bb6fbfba5305424610beeab5a9016c697a"
+      url "https://github.com/sadopc/gottp/releases/download/v1.0.0/gottp_1.0.0_darwin_arm64.tar.gz"
+      sha256 "0fb345966607eea95d12835359ca64763e9899916140f437b75f5368bda2b57b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_linux_amd64.tar.gz"
-      sha256 "166423bcfe310a8f32802d2c08b5ea8ed016190c81e5138af425bdc1e519cb25"
+      url "https://github.com/sadopc/gottp/releases/download/v1.0.0/gottp_1.0.0_linux_amd64.tar.gz"
+      sha256 "6e211e13a357640f41007edde965f149050a9eadfdbeb16187aef2ccf428f625"
     end
     on_arm do
-      url "https://github.com/sadopc/gottp/releases/download/v0.5.0/gottp_0.5.0_linux_arm64.tar.gz"
-      sha256 "42e062eb182c1207450042e97128455c1a9e7ea9523ebb65e0eee9571d885151"
+      url "https://github.com/sadopc/gottp/releases/download/v1.0.0/gottp_1.0.0_linux_arm64.tar.gz"
+      sha256 "7fb4d6eab488e29006bcf6924b57e410e939749e22a3436af9ad34a0ff823d95"
     end
   end
 
