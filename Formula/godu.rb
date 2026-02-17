b96a87e9e3403574470dc7593500cd70e7f92750
sadopc <98687378+sadopc@users.noreply.github.com>
2026-02-17 19:09:03 +0300

    Update godu to v1.2.9

--- Formula/godu.rb
+++ Formula/godu.rb
@@ -1,26 +1,26 @@
 class Godu < Formula
   desc "Fast, interactive disk usage analyzer for the terminal"
   homepage "https://github.com/sadopc/godu"
-  version "1.2.8"
+  version "1.2.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sadopc/godu/releases/download/v1.2.8/godu-darwin-arm64"
-      sha256 "c967ff8c7bb616d3e62dcff7ab0952b634d161859062355d4988c6df14296208"
+      url "https://github.com/sadopc/godu/releases/download/v1.2.9/godu-darwin-arm64"
+      sha256 "2217969ebd98fc1911d98b5cf016433b3f9fe91b080c8e5844032ce518d24480"
     else
-      url "https://github.com/sadopc/godu/releases/download/v1.2.8/godu-darwin-amd64"
-      sha256 "4ac7c484452ff1cd51c30bffc77f8d746e3d7e1767b9959742ea9ade18fc148b"
+      url "https://github.com/sadopc/godu/releases/download/v1.2.9/godu-darwin-amd64"
+      sha256 "4958fed1f848ccc273d6056e1a6efc148122a004c8b9da968cb9318e093bfbdf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sadopc/godu/releases/download/v1.2.8/godu-linux-arm64"
-      sha256 "9d456fe9fb3f5e2edd4ee8bd168455ea50a61248ee541bfadbd298170eb3474b"
+      url "https://github.com/sadopc/godu/releases/download/v1.2.9/godu-linux-arm64"
+      sha256 "79eee55f63456938e739962f65cb21ae3c855b485a4557be5c2d74ba095ff634"
     else
-      url "https://github.com/sadopc/godu/releases/download/v1.2.8/godu-linux-amd64"
-      sha256 "95c806ea0ca198b96bbc83ea48189456931aa85e62084b1a805d034f1bd3ed8c"
+      url "https://github.com/sadopc/godu/releases/download/v1.2.9/godu-linux-amd64"
+      sha256 "2f35c5b9da5dc782c7bec30b47b05345e4307edaa4f95c909d051cede74784c1"
     end
   end
 
